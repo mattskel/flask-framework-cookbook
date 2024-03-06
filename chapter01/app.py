4976b6553160cb7b894b7b353371b1264750dd01
Matt Skelley <mattskelley.au@gmail.com>
2024-03-06 22:21:27 +1100

    Flask configurations

--- chapter01/app.py
+++ chapter01/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 # from configuration import DevelopmentConfig
 
-app = Flask(__name__)
+# Load the instance from the instance folder
+app = Flask(__name__,
+        static_folder='/static',
+        instance_relative_config=True)
 # app.config['DEBUG'] = True
 # app.config.from_object(DevelopmentConfig)
-app.config.from_object('configuration.DevelopmentConfig') # Using the module path
+# app.config.from_object('configuration.DevelopmentConfig') # Using the module path
+app.config.from_pyfile('config.cfg')
 
 @app.route('/')
 def hello_world():
